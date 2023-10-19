18722b48d0459c5a544b6fe2f43c90517cee311e
azubillagasdam1 <azubillagasdam1@gmail.com>
2023-10-19 11:47:54 +0200

    Mensaje del commit autom├ítico

--- guardar.bat
+++ guardar.bat
@@ -1,18 +1,21 @@
 @echo off
 setlocal
 
-set REPO_DIR=C:\Users\alumMA\JuegoCasa
-set COMMIT_MESSAGE="Mensaje del commit automático"
+set "SCRIPT_DIR=%~dp0"
+set "REPO_DIR=%SCRIPT_DIR%"
+set "COMMIT_MESSAGE=Mensaje del commit automático"
 
 cd /d "%REPO_DIR%"
 
 echo Realizando commit...
 git add .
-git commit -m %COMMIT_MESSAGE%
+git commit -m "%COMMIT_MESSAGE%"
 
 echo Realizando push...
 git push
 
 echo Proceso completado.
+echo "Presiona una tecla para continuar..."
+pause
 
 endlocal
