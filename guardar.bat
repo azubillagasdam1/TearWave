3a00e6d66e05d3ca2baadb6ab1e6e09fb72b37f6
azubillagasdam1 <azubillagasdam1@gmail.com>
2023-10-30 14:09:06 +0100

    Mensaje del commit autom├ítico

--- guardar.bat
+++ guardar.bat
@@ -1,5 +1,6 @@
 @echo off
 setlocal
+mode con cols=160 lines=60
 color 0a
 
 set "SCRIPT_DIR=%~dp0"
@@ -18,11 +19,5 @@ git push
 echo Proceso completado.
 echo "Presiona una tecla para continuar..."
 pause
-
-mode con cols=160 lines=60
-echo.
-echo La ventana de la consola está en pantalla completa. Presiona una tecla para restaurar el tamaño original.
-pause > nul
-mode con cols=80 lines=25
 
 endlocal
