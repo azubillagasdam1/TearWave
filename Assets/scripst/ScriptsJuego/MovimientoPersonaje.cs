4ff5a8e2bcd7ca9dc0335721b34895a10303f6c0
azubillagasdam1 <azubillagasdam1@gmail.com>
2023-11-13 14:09:09 +0100

    Mensaje del commit autom├ítico

--- Assets/scripst/ScriptsJuego/MovimientoPersonaje.cs
+++ Assets/scripst/ScriptsJuego/MovimientoPersonaje.cs
@@ -4,36 +4,42 @@ public class MovimientoPersonaje : MonoBehaviour
 {
     public float fuerzaArriba = 10f; // Fuerza hacia arriba
     public float fuerzaAbajo = -10f; // Fuerza hacia abajo
+    private bool tocandoSuelo = false;
     private Rigidbody2D rb;
-   
+
     public bool vivo;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         vivo = true;
-       
     }
 
     void Update()
     {
         // Si se mantiene presionado el botón izquierdo del mouse
-       
- 
-
-    if (Input.GetMouseButton(0))
-    {
-        AplicarFuerzaArriba();
+        if (Input.GetMouseButton(0))
+        {
+             Debug.Log("Subiendo");
+            AplicarFuerzaArriba();
+        }
+        else
+        {
+            if (!tocandoSuelo)
+            {
+                Debug.Log("Bajando");
+                AplicarFuerzaAbajo();
+            }
+            else
+            {
+                rb.velocity = new Vector2(rb.velocity.x, 0f);
+                   Debug.Log("Hechado");
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+        }
     }
-    else
-    {
-        AplicarFuerzaAbajo();
-    }
 
-
-    }
-    
-
+            
     void AplicarFuerzaArriba()
     {
         rb.velocity = new Vector2(rb.velocity.x, fuerzaArriba);
@@ -48,8 +54,6 @@ public class MovimientoPersonaje : MonoBehaviour
         transform.rotation = Quaternion.Euler(0, 0, -45);
     }
 
- 
-
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Obstaculo"))
@@ -60,32 +64,22 @@ public class MovimientoPersonaje : MonoBehaviour
         }
         if (collision.gameObject.CompareTag("Bordes"))
         {
-            // Elimina el personaje si colisiona con el Tilemap
-            
-          rb.velocity = new Vector2(rb.velocity.x, 0f);
-        // Restablecer la rotación en el eje Z a 0 grados
-        transform.rotation = Quaternion.Euler(0, 0, 0);
+            // Establecer tocandoSuelo como false cuando ya no detecta colisiones con los bordes
+            tocandoSuelo = true;
         }
     }
-     private void OnTriggerEnter2D(Collider2D collision)
+
+    void OnCollisionExit2D(Collision2D collision)
     {
-    if (collision.gameObject.CompareTag("Obstaculo"))
-        {
-            // Elimina el personaje si colisiona con el Tilemap
-            Destroy(gameObject);
-            vivo = false;
-        }
         if (collision.gameObject.CompareTag("Bordes"))
         {
-             rb.velocity = new Vector2(rb.velocity.x, 0f);
-        // Restablecer la rotación en el eje Z a 0 grados
-        transform.rotation = Quaternion.Euler(0, 0, 0);
+            // Establecer tocandoSuelo como false cuando ya no detecta colisiones con los bordes
+            tocandoSuelo = false;
         }
-
     }
-    public bool getVivo(){
+
+    public bool getVivo()
+    {
         return vivo;
     }
-
-
 }
